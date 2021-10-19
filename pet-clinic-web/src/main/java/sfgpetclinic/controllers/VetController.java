de06e733811cc58d6033380cd9af0ab775d28310
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-10-19 12:41:35 +0200

    broken links fixed

--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetservice = vetservice;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    @RequestMapping({"/vets","/vets/index","/vets/index.html", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetservice.findAll());
 
