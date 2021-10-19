06d89a0c83a8a7f3040a49bc91806e988b3dfb73
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-10-19 12:23:51 +0200

    specialties id's

--- pet-clinic-data/src/main/java/sfgpetclinic/services/Map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/Map/VetServiceMap.java
@@ -1,13 +1,20 @@
 package sfgpetclinic.services.Map;
 
 import org.springframework.stereotype.Service;
+import sfgpetclinic.model.Specialty;
 import sfgpetclinic.model.Vet;
-import sfgpetclinic.services.CrudService;
+import sfgpetclinic.services.SpecialtiesService;
 import sfgpetclinic.services.Vetservice;
 
 import java.util.Set;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements Vetservice {
+    private final SpecialtiesService specialtiesService;
+
+    public VetServiceMap(SpecialtiesService specialtiesService) {
+        this.specialtiesService = specialtiesService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -27,6 +34,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements Vets
 
     @Override
     public Vet save(Vet object) {
+
+        if(object.getSpecialties().size()>0){
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtiesService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
