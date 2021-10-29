cf91ae04ae7f59ffb9269f2dbdde273307c6e913
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-10-29 08:05:02 +0200

    lombok updates

---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,14 @@
+package sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
